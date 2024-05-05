f92b131c14874466bc3e2a34adb0274bf3eb4945
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-05-05 20:46:32 -0300

    .

--- dist/repositories/ContactRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data_source_1 = require("../config/data-source");
-const Contact_entity_1 = require("../entities/Contact.entity");
-const ContactRepository = data_source_1.AppDataSource.getRepository(Contact_entity_1.Contact);
-exports.default = ContactRepository;
-//# sourceMappingURL=ContactRepository.js.map
